65ba0fda228a88e5a6b639d02867cc28e90b8877
Pravin Satpute <psatpute@redhat.com>
2014-06-29 00:47:26 +0530

    UTF-8 now working correctly

--- utf8-gen.py
+++ utf8-gen.py
@@ -21,30 +21,28 @@
 import os
 import sys		
 
+''' Where UnicodeData.txt file has given characters in range i.e. First> and Last>
+   UTf-8 file write its by adding 0x3F inbetween First and Last Unicode character.
+'''
+# No idea why Hangul syllable AC00; D7A3; are not expanded. We are following same rule for all.
 def process_range(start, end, outfile, name):
-#	print "start:", start
-#	print "\nend:", end
+
 	for i in range(int(start, 16), int(end, 16), 64 ):
 		unihex = unichr(i).encode("UTF-8")
-         	length_hex = len(unihex)
-         	hexword = ""
-         	for x in range(0, length_hex):
-         		hexword =hexword + "/x" + ('%x' % ord(unihex[x]))
-		if len(start) == 4:
-			outfile.write("<U"+('%x' % i).upper()+">.." +  "<U"+('%x' % (i+63)).upper()+">     " + hexword + "         " + name.split(",")[0] + ">" + "\n")
-		else:
-			outfile.write("<U000"+('%x' % i).upper()+">.." +  "<U000"+('%x' % (i+63)).upper()+">     " + hexword + "         " + name.split(",")[0] + ">" + "\n")
-		
+		hexword = convert_to_hex(unihex)
+
 		if i > (int(end, 16)-64):
 			if len(start) == 4:
 				outfile.write("<U"+('%x' % i).upper()+">.." +  "<U"+('%x' % int(end, 16)).upper()+">     " + hexword + "         " + name.split(",")[0] + ">" + "\n")
 			else:
 				outfile.write("<U000"+('%x' % i).upper()+">.." +  "<U000"+('%x' % int(end, 16)).upper()+">     " + hexword + "         " + name.split(",")[0] + ">" + "\n")
-
 			break
 
-#	print start
-#	print end
+		if len(start) == 4:
+			outfile.write("<U"+('%x' % i).upper()+">.." +  "<U"+('%x' % (i+63)).upper()+">     " + hexword + "         " + name.split(",")[0] + ">" + "\n")
+		else:
+			outfile.write("<U000"+('%x' % i).upper()+">.." +  "<U000"+('%x' % (i+63)).upper()+">     " + hexword + "         " + name.split(",")[0] + ">" + "\n")
+		
 
 def process_unidata(flines, outfile):
 	l = 0
@@ -52,20 +50,24 @@ def process_unidata(flines, outfile):
 		w = flines[l].split(";")
 
 		# Getting UTF8 of Unicode characters
-                unihex = unichr(int(w[0],16)).encode("UTF-8")
-		length_hex = len(unihex)
-		hexword = ""
-                for i in range(0, length_hex):
-                      hexword =hexword + "/x" + ('%x' % ord(unihex[i]))
+		unihex = unichr(int(w[0],16)).encode("UTF-8")
+		hexword = convert_to_hex(unihex)
 
 		# Some characters have <control> as a name, so using "Unicode 1.0 Name"  
+		#  Characters U+0080, U+0081, U+0084 and U+0099 has "<control>" as a name and even no "Unicode 1.0 Name" (10th field) in UnicodeData.txt
+		#  We can write code to take there alternate name from NameAliases.txt
 		if w[1] == "<control>":
-			if w[10] == "":
-			#   4 characters U+0080, U+0081, U+0084 and U+0099 has "<control>" as a name and even no "Unicode 1.0 Name" (10th field) in UnicodeData.txt
-			#   We can write code to take there alternate name from NameAliases.txt
-				print "No alternate"
-			else:
+			if w[10] != "":
 			       w[1] = w[10]
+
+		# Surrogates are disabled in UTF-8 file
+		if w[1].find("Surrogate,")!=-1:
+			if len(w[0]) == 4:
+				outfile.write("%<U"+w[0]+">     " + hexword + "         " + w[1] + "\n")
+			else:
+				outfile.write("%<U000"+w[0]+">     " + hexword + "         " + w[1] + "\n")
+			l = l +1
+			continue
 
 		# Handling case of CJK IDEAOGRAPH Start (3400) and End(4DB5), ADD 0x3F and create range. some more cases like this
 		if w[1].find(", First>")!=-1:
@@ -81,6 +83,16 @@ def process_unidata(flines, outfile):
 		    outfile.write("<U000"+w[0]+">     " + hexword + "         " + w[1] + "\n")
 		l = l +1
 
+''' Function to convert Unicode characters to /x**/x**/x** 
+   format. 
+'''
+def convert_to_hex(unihex):
+	length_hex = len(unihex)
+	hexword = ""
+        for i in range(0, length_hex):
+              hexword =hexword + "/x" + ('%x' % ord(unihex[i]))
+	return hexword
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print  "USAGE: python utf8-gen.py UnicodeData.txt"
@@ -94,7 +106,9 @@ if __name__ == "__main__":
 	outfile.write("<escape_char> /\n")
 	outfile.write("<mb_cur_min> 1\n")
 	outfile.write("<mb_cur_max> 6\n\n")
-	# Processing UnicodeData.txt
+	outfile.write("% alias ISO-10646/UTF-8\n")
+	outfile.write("CHARMAP\n")
+	# Processing UnicodeData.txt and write to UTF-8 file
 	process_unidata(flines, outfile)
 
         outfile.close()
