ccc0fa3945ee2a9f19ce01cb5b8555df69b5e26f
Mike FABIAN <mfabian@redhat.com>
2014-10-10 12:53:20 +0200

    Convert all tabs to spaces
        $ python3 utf8-gen.py ~/Downloads/UCD/UnicodeData.txt ~/Downloads/UCD/EastAsianWidth.txt
          File "utf8-gen.py", line 119
            for i in range(0, length_hex):
                                         ^
        TabError: inconsistent use of tabs and spaces in indentation
    
        http://legacy.python.org/dev/peps/pep-0008/#tabs-or-spaces
    
        > Python 3 disallows mixing the use of tabs and spaces for indentation.

--- utf8-compatibility.py
+++ utf8-compatibility.py
@@ -24,105 +24,105 @@
 import sys
 
 def check_charmap(original, new):
-	ocharmap = {}
-	ncharmap = {}
-	i = 0
-	for l in new:
-		w = l.split()
-		if len(w) > 0:
-			if w[0] == "CHARMAP":
-				create_dict(ncharmap, new, i+1)
-				break
-		i = i + 1
+        ocharmap = {}
+        ncharmap = {}
+        i = 0
+        for l in new:
+                w = l.split()
+                if len(w) > 0:
+                        if w[0] == "CHARMAP":
+                                create_dict(ncharmap, new, i+1)
+                                break
+                i = i + 1
 
-	i = 0
-	for l in original:
-		w = l.split()
-		if len(w) > 0:
-#			print w[0]
-			if w[0] == "CHARMAP":
-				for x in range(i+1, len(original)):
-					w = original[x].split()
-					if w[0] == "END":
-						break
-#					print w[0]
-					try:
-						if ncharmap[w[0]] != w[1]:
-							print "This character might be missing in generated charmap: ", w[0]
-					except KeyError:
-						if  w[0] !='%':
-							print "This character might be missing in new generated charmap: ", w[0]
-		i = i + 1
+        i = 0
+        for l in original:
+                w = l.split()
+                if len(w) > 0:
+#                       print w[0]
+                        if w[0] == "CHARMAP":
+                                for x in range(i+1, len(original)):
+                                        w = original[x].split()
+                                        if w[0] == "END":
+                                                break
+#                                       print w[0]
+                                        try:
+                                                if ncharmap[w[0]] != w[1]:
+                                                        print "This character might be missing in generated charmap: ", w[0]
+                                        except KeyError:
+                                                if  w[0] !='%':
+                                                        print "This character might be missing in new generated charmap: ", w[0]
+                i = i + 1
 
 
 def create_dict(name, lines, i):
-	for x in range(i, len(lines)):
-		w = lines[x].split()
-		if w[0] == "END":
-			break
-		name[w[0]] = w[1]
-#	print name
+        for x in range(i, len(lines)):
+                w = lines[x].split()
+                if w[0] == "END":
+                        break
+                name[w[0]] = w[1]
+#       print name
 
 
 def process_chars(line_no, lines, dictionary):
-	for x in range(line_no, len(lines)):
-		w = lines[x].split()
-		if w[0] == "END":
-			break
-		if w[0].find("...")==-1:
-			uni_char = w[0][2:len(w[0])-1]
-			hex_uni_char = hex(int(uni_char,16))
-#			print hex_uni_char, w[1]
-			dictionary[hex_uni_char]= w[1]
-		else:
-			wc = w[0].split("...")
-#			print wc
-			uni_char1 = wc[0][2:len(wc[0])-1]
-			hex_uni_char1 = hex(int(uni_char1,16))
-			uni_char2 = wc[1][2:len(wc[0])-1]
-			hex_uni_char2 = hex(int(uni_char2,16))
-#			print hex_uni_char1, hex_uni_char2
-			count = 0
-			for i in range (int(uni_char1,16), int(uni_char2,16)+1):
-				dictionary[hex(int(uni_char1,16)+count)] = w[1]
-				count = count +1
+        for x in range(line_no, len(lines)):
+                w = lines[x].split()
+                if w[0] == "END":
+                        break
+                if w[0].find("...")==-1:
+                        uni_char = w[0][2:len(w[0])-1]
+                        hex_uni_char = hex(int(uni_char,16))
+#                       print hex_uni_char, w[1]
+                        dictionary[hex_uni_char]= w[1]
+                else:
+                        wc = w[0].split("...")
+#                       print wc
+                        uni_char1 = wc[0][2:len(wc[0])-1]
+                        hex_uni_char1 = hex(int(uni_char1,16))
+                        uni_char2 = wc[1][2:len(wc[0])-1]
+                        hex_uni_char2 = hex(int(uni_char2,16))
+#                       print hex_uni_char1, hex_uni_char2
+                        count = 0
+                        for i in range (int(uni_char1,16), int(uni_char2,16)+1):
+                                dictionary[hex(int(uni_char1,16)+count)] = w[1]
+                                count = count +1
 
-#	print dictionary
+#       print dictionary
 
 
 def extract_univalue_and_width(lines, dictionary):
         i = 0
-	for l in lines:
-		w = l.split()
-		if len(w) > 0:
-#			print w[0]
-	    		if w[0] == "WIDTH":
-				process_chars(i+1, lines, dictionary)
-				break
-		i = i + 1
+        for l in lines:
+                w = l.split()
+                if len(w) > 0:
+#                       print w[0]
+                        if w[0] == "WIDTH":
+                                process_chars(i+1, lines, dictionary)
+                                break
+                i = i + 1
 
 def check_width(olines, nlines):
-	owidth = {}
-	nwidth = {}
-	extract_univalue_and_width(olines, owidth)
-	extract_univalue_and_width(nlines, nwidth)
-	mwidth = dict(set(owidth.iteritems()) - set(owidth.iteritems()).intersection(nwidth.iteritems()))
-	print "Total missing characters in newly generated WIDTH: ", len(mwidth)
-	for key, value in sorted(mwidth.items()):
-		print("{} : {}".format(key, value))
+        owidth = {}
+        nwidth = {}
+        extract_univalue_and_width(olines, owidth)
+        extract_univalue_and_width(nlines, nwidth)
+        mwidth = dict(set(owidth.iteritems()) - set(owidth.iteritems()).intersection(nwidth.iteritems()))
+        print "Total missing characters in newly generated WIDTH: ", len(mwidth)
+        for key, value in sorted(mwidth.items()):
+                print("{} : {}".format(key, value))
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print  "USAGE: python check_backcompatibility existing_ctype_file new_ctype_file "
     else:
-	# o_ for Original UTF-8 and n_ for New UTF-8 file
-	o_utf8 = open(sys.argv[1])
+        # o_ for Original UTF-8 and n_ for New UTF-8 file
+        o_utf8 = open(sys.argv[1])
         n_utf8 = open(sys.argv[2])
-	o_lines = o_utf8.readlines()
-	n_lines = n_utf8.readlines()
+        o_lines = o_utf8.readlines()
+        n_lines = n_utf8.readlines()
         print  "Report on CHARMAP:"
-	check_charmap(o_lines, n_lines)
-	print  "************************************************************\n"
+        check_charmap(o_lines, n_lines)
+        print  "************************************************************\n"
         print  "Report on WIDTH:"
-	check_width(o_lines, n_lines)
+        check_width(o_lines, n_lines)
--- utf8-gen.py
+++ utf8-gen.py
@@ -41,109 +41,109 @@ import os,sys,re
    ranges.  Add a WIDTH table.
 '''
 def process_range(start, end, outfile, name):
-	if name.find("Hangul Syllable")!=-1:
-		for i in range(int(start, 16), int(end, 16)+1 ):
-			unihex = unichr(i).encode("UTF-8")
-			hexword = convert_to_hex(unihex)
-			outfile.write("<U"+('%x' % i).upper()+">     " + hexword + "         " + name.split(",")[0] + ">" + "\n")
+        if name.find("Hangul Syllable")!=-1:
+                for i in range(int(start, 16), int(end, 16)+1 ):
+                        unihex = unichr(i).encode("UTF-8")
+                        hexword = convert_to_hex(unihex)
+                        outfile.write("<U"+('%x' % i).upper()+">     " + hexword + "         " + name.split(",")[0] + ">" + "\n")
 
 
-	else:
-		for i in range(int(start, 16), int(end, 16), 64 ):
-			unihex = unichr(i).encode("UTF-8")
-			hexword = convert_to_hex(unihex)
+        else:
+                for i in range(int(start, 16), int(end, 16), 64 ):
+                        unihex = unichr(i).encode("UTF-8")
+                        hexword = convert_to_hex(unihex)
 
-			if i > (int(end, 16)-64):
-				if len(start) == 4:
-					outfile.write("<U"+('%x' % i).upper()+">.." +  "<U"+('%x' % int(end, 16)).upper()+">     " + hexword + "         " + name.split(",")[0] + ">" + "\n")
-				elif len(start) == 5:
-					outfile.write("<U000"+('%x' % i).upper()+">.." +  "<U000"+('%x' % int(end, 16)).upper()+">     " + hexword + "         " + name.split(",")[0] + ">" + "\n")
-				else:
-					outfile.write("<U00"+('%x' % i).upper()+">.." +  "<U00"+('%x' % int(end, 16)).upper()+">     " + hexword + "         " + name.split(",")[0] + ">" + "\n")
-				break
+                        if i > (int(end, 16)-64):
+                                if len(start) == 4:
+                                        outfile.write("<U"+('%x' % i).upper()+">.." +  "<U"+('%x' % int(end, 16)).upper()+">     " + hexword + "         " + name.split(",")[0] + ">" + "\n")
+                                elif len(start) == 5:
+                                        outfile.write("<U000"+('%x' % i).upper()+">.." +  "<U000"+('%x' % int(end, 16)).upper()+">     " + hexword + "         " + name.split(",")[0] + ">" + "\n")
+                                else:
+                                        outfile.write("<U00"+('%x' % i).upper()+">.." +  "<U00"+('%x' % int(end, 16)).upper()+">     " + hexword + "         " + name.split(",")[0] + ">" + "\n")
+                                break
 
-			if len(start) == 4:
-				outfile.write("<U"+('%x' % i).upper()+">.." +  "<U"+('%x' % (i+63)).upper()+">     " + hexword + "         " + name.split(",")[0] + ">" + "\n")
-			elif len(start) == 5:
-				outfile.write("<U000"+('%x' % i).upper()+">.." +  "<U000"+('%x' % (i+63)).upper()+">     " + hexword + "         " + name.split(",")[0] + ">" + "\n")
-			else:
-				outfile.write("<U00"+('%x' % i).upper()+">.." +  "<U00"+('%x' % (i+63)).upper()+">     " + hexword + "         " + name.split(",")[0] + ">" + "\n")
+                        if len(start) == 4:
+                                outfile.write("<U"+('%x' % i).upper()+">.." +  "<U"+('%x' % (i+63)).upper()+">     " + hexword + "         " + name.split(",")[0] + ">" + "\n")
+                        elif len(start) == 5:
+                                outfile.write("<U000"+('%x' % i).upper()+">.." +  "<U000"+('%x' % (i+63)).upper()+">     " + hexword + "         " + name.split(",")[0] + ">" + "\n")
+                        else:
+                                outfile.write("<U00"+('%x' % i).upper()+">.." +  "<U00"+('%x' % (i+63)).upper()+">     " + hexword + "         " + name.split(",")[0] + ">" + "\n")
 ''' This function takes single like of UnicodeData.txt and write to UTF-8
     Unicode-Value  HEX  Unicode-Char-Name
     <U0010>     /x10         DATA LINK ESCAPE
 '''
 def process_charmap(flines, outfile):
-	l = 0
-	while l < len(flines):
-		w = flines[l].split(";")
+        l = 0
+        while l < len(flines):
+                w = flines[l].split(";")
 
-		# Getting UTF8 of Unicode characters
-		unihex = unichr(int(w[0],16)).encode("UTF-8")
-		hexword = convert_to_hex(unihex)
+                # Getting UTF8 of Unicode characters
+                unihex = unichr(int(w[0],16)).encode("UTF-8")
+                hexword = convert_to_hex(unihex)
 
-		''' Some characters have <control> as a name, so using "Unicode 1.0 Name"
-		    Characters U+0080, U+0081, U+0084 and U+0099 has "<control>" as a name and even no "Unicode 1.0 Name" (10th field) in UnicodeData.txt
-		    We can write code to take there alternate name from NameAliases.txt '''
-		if w[1] == "<control>":
-			if w[10] != "":
-			       w[1] = w[10]
+                ''' Some characters have <control> as a name, so using "Unicode 1.0 Name"
+                    Characters U+0080, U+0081, U+0084 and U+0099 has "<control>" as a name and even no "Unicode 1.0 Name" (10th field) in UnicodeData.txt
+                    We can write code to take there alternate name from NameAliases.txt '''
+                if w[1] == "<control>":
+                        if w[10] != "":
+                               w[1] = w[10]
 
-		# Surrogates are disabled in UTF-8 file
-		if w[1].find("Surrogate,")!=-1:
-			if len(w[0]) == 4:
-				outfile.write("%<U"+w[0]+">     " + hexword + "         " + w[1] + "\n")
-			else:
-				outfile.write("%<U000"+w[0]+">     " + hexword + "         " + w[1] + "\n")
-			l = l +1
-			continue
+                # Surrogates are disabled in UTF-8 file
+                if w[1].find("Surrogate,")!=-1:
+                        if len(w[0]) == 4:
+                                outfile.write("%<U"+w[0]+">     " + hexword + "         " + w[1] + "\n")
+                        else:
+                                outfile.write("%<U000"+w[0]+">     " + hexword + "         " + w[1] + "\n")
+                        l = l +1
+                        continue
 
-		# Handling case of CJK IDEAOGRAPH Start (3400) and End(4DB5), ADD 0x3F and create range. some more cases like this
-		if w[1].find(", First>")!=-1:
-			start = w[0]
-			end = flines[l+1].split(";")[0]
-			process_range(start, end, outfile, w[1])
-			l = l +2
-			continue
+                # Handling case of CJK IDEAOGRAPH Start (3400) and End(4DB5), ADD 0x3F and create range. some more cases like this
+                if w[1].find(", First>")!=-1:
+                        start = w[0]
+                        end = flines[l+1].split(";")[0]
+                        process_range(start, end, outfile, w[1])
+                        l = l +2
+                        continue
 
-		if len(w[0]) == 4:
-		    outfile.write("<U"+w[0]+">     " + hexword + "         " + w[1] + "\n")
-		else:
-		    outfile.write("<U000"+w[0]+">     " + hexword + "         " + w[1] + "\n")
-		l = l +1
+                if len(w[0]) == 4:
+                    outfile.write("<U"+w[0]+">     " + hexword + "         " + w[1] + "\n")
+                else:
+                    outfile.write("<U000"+w[0]+">     " + hexword + "         " + w[1] + "\n")
+                l = l +1
 
 ''' Function to convert Unicode characters to /x**/x**/x**  format.
 '''
 def convert_to_hex(unihex):
-	length_hex = len(unihex)
-	hexword = ""
+        length_hex = len(unihex)
+        hexword = ""
         for i in range(0, length_hex):
               hexword =hexword + "/x" + ('%x' % ord(unihex[i]))
-	return hexword
+        return hexword
 
 def write_comments(outfile, flag):
-	if flag == 0:
-	        outfile.write("<code_set_name> UTF-8\n")
-		outfile.write("<comment_char> %\n")
-		outfile.write("<escape_char> /\n")
-		outfile.write("<mb_cur_min> 1\n")
-		outfile.write("<mb_cur_max> 6\n\n")
-		outfile.write("% CHARMAP generated using utf8-gen.py\n")
-		outfile.write("% alias ISO-10646/UTF-8\n")
-		outfile.write("CHARMAP\n")
-	if flag == 1:
-		outfile.write("% Character width according to Unicode 7.0.0.\n")
-		outfile.write("% - Default width is 1.\n")
-		outfile.write("% - Double-width characters have width 2; generated from\n")
-		outfile.write("%        \"grep '^[^;]*;[WF]' EastAsianWidth.txt\"\n")
-#		outfile.write("%   and  \"grep '^[^;]*;[^WF]' EastAsianWidth.txt\"\n")  -- This is wrong
-		outfile.write("% - Non-spacing characters have width 0; generated from PropList.txt or\n")
-		outfile.write("%   \"grep '^[^;]*;[^;]*;[^;]*;[^;]*;NSM;' UnicodeData.txt\"\n")
-		outfile.write("% - Format control characters have width 0; generated from\n")
-		outfile.write("%   \"grep '^[^;]*;[^;]*;Cf;' UnicodeData.txt\"\n")
+        if flag == 0:
+                outfile.write("<code_set_name> UTF-8\n")
+                outfile.write("<comment_char> %\n")
+                outfile.write("<escape_char> /\n")
+                outfile.write("<mb_cur_min> 1\n")
+                outfile.write("<mb_cur_max> 6\n\n")
+                outfile.write("% CHARMAP generated using utf8-gen.py\n")
+                outfile.write("% alias ISO-10646/UTF-8\n")
+                outfile.write("CHARMAP\n")
+        if flag == 1:
+                outfile.write("% Character width according to Unicode 7.0.0.\n")
+                outfile.write("% - Default width is 1.\n")
+                outfile.write("% - Double-width characters have width 2; generated from\n")
+                outfile.write("%        \"grep '^[^;]*;[WF]' EastAsianWidth.txt\"\n")
+#               outfile.write("%   and  \"grep '^[^;]*;[^WF]' EastAsianWidth.txt\"\n")  -- This is wrong
+                outfile.write("% - Non-spacing characters have width 0; generated from PropList.txt or\n")
+                outfile.write("%   \"grep '^[^;]*;[^;]*;[^;]*;[^;]*;NSM;' UnicodeData.txt\"\n")
+                outfile.write("% - Format control characters have width 0; generated from\n")
+                outfile.write("%   \"grep '^[^;]*;[^;]*;Cf;' UnicodeData.txt\"\n")
 #             Not needed covered by Cf
-#		outfile.write("% - Zero width characters have width 0; generated from\n")
-#		outfile.write("%   \"grep '^[^;]*;ZERO WIDTH ' UnicodeData.txt\"\n")
-		outfile.write("WIDTH\n")
+#               outfile.write("% - Zero width characters have width 0; generated from\n")
+#               outfile.write("%   \"grep '^[^;]*;ZERO WIDTH ' UnicodeData.txt\"\n")
+                outfile.write("WIDTH\n")
 
 
 ''' For WIDTH we need to process output from 2 files UnicodeData.txt and EastAsianWidth.txt.
@@ -153,42 +153,42 @@ def write_comments(outfile, flag):
    4. Removing temporary files.
 '''
 def process_width(outfile, ulines, elines):
-	ftmp = open("temp", "w")
-	elist = []
-	for l in ulines:
-		w = l.split(";")
-		if w[4]== "NSM" or w[2] == "Cf":
-			if len(w[0]) < 5:
-				elist.append(str(int(w[0],16)) + "\t" + "<U"+w[0]+">\t\t\t0" + "\n")
-			else:
-				elist.append(str(int(w[0],16)) + "\t" + "<U000"+w[0]+">\t\t\t0" + "\n")
-#			print w[0]
+        ftmp = open("temp", "w")
+        elist = []
+        for l in ulines:
+                w = l.split(";")
+                if w[4]== "NSM" or w[2] == "Cf":
+                        if len(w[0]) < 5:
+                                elist.append(str(int(w[0],16)) + "\t" + "<U"+w[0]+">\t\t\t0" + "\n")
+                        else:
+                                elist.append(str(int(w[0],16)) + "\t" + "<U000"+w[0]+">\t\t\t0" + "\n")
+#                       print w[0]
 
-	for l in elines:
-		w = l.split(";")
-		if len(w[0])<6:
-			if len(w[0]) == 4:
-				elist.append(str(int(w[0],16)) + "\t" + "<U"+w[0]+">\t\t\t2" + "\n")
-			else:
-				elist.append(str(int(w[0],16)) + "\t" + "<U000"+w[0]+">\t\t\t2" + "\n")
-		else:
-			wc = w[0].split("..")
-			if len(wc[0]) == 4:
-				elist.append(str(int(wc[0],16)) + "\t" + "<U"+wc[0]+">..." + "<U"+wc[1]+">\t2\n" )
-			else:
-				elist.append(str(int(wc[0],16)) + "\t" + "<U000"+wc[0]+">..." + "<U000"+wc[1]+">\t2\n")
+        for l in elines:
+                w = l.split(";")
+                if len(w[0])<6:
+                        if len(w[0]) == 4:
+                                elist.append(str(int(w[0],16)) + "\t" + "<U"+w[0]+">\t\t\t2" + "\n")
+                        else:
+                                elist.append(str(int(w[0],16)) + "\t" + "<U000"+w[0]+">\t\t\t2" + "\n")
+                else:
+                        wc = w[0].split("..")
+                        if len(wc[0]) == 4:
+                                elist.append(str(int(wc[0],16)) + "\t" + "<U"+wc[0]+">..." + "<U"+wc[1]+">\t2\n" )
+                        else:
+                                elist.append(str(int(wc[0],16)) + "\t" + "<U000"+wc[0]+">..." + "<U000"+wc[1]+">\t2\n")
 
-	for i in range(len(elist)):
-		ftmp.write(elist[i])
-	ftmp.close()
-	os.system("sort -n temp > sorted_temp")
-	#writing to UTF-8 file
-	ftmp = open("sorted_temp")
-	tlines = ftmp.readlines()
-	for l in tlines:
-		w = l.split()
-		outfile.write(w[1] + "\t" + w[2] + "\n")
-	os.system("rm temp sorted_temp")
+        for i in range(len(elist)):
+                ftmp.write(elist[i])
+        ftmp.close()
+        os.system("sort -n temp > sorted_temp")
+        #writing to UTF-8 file
+        ftmp = open("sorted_temp")
+        tlines = ftmp.readlines()
+        for l in tlines:
+                w = l.split()
+                outfile.write(w[1] + "\t" + w[2] + "\n")
+        os.system("rm temp sorted_temp")
 
 
 
@@ -199,21 +199,21 @@ if __name__ == "__main__":
         unidata_file = open(sys.argv[1])
         easta_file = open(sys.argv[2])
         outfile=open("UTF-8","w")
-	flines = unidata_file.readlines()
+        flines = unidata_file.readlines()
 
-	# Processing UnicodeData.txt and write CHARMAP to UTF-8 file
-	write_comments(outfile, 0)
-	process_charmap(flines, outfile)
-	outfile.write("END CHARMAP\n\n")
+        # Processing UnicodeData.txt and write CHARMAP to UTF-8 file
+        write_comments(outfile, 0)
+        process_charmap(flines, outfile)
+        outfile.write("END CHARMAP\n\n")
 
-	# Processing EastAsianWidth.txt and write WIDTH to UTF-8 file
-	write_comments(outfile, 1)
+        # Processing EastAsianWidth.txt and write WIDTH to UTF-8 file
+        write_comments(outfile, 1)
         elines = []
         for line in easta_file.readlines():
                 if re.match(r'^[^;]*;[WF]', line):
                         elines.append(line.strip())
-	process_width(outfile, flines, elines)
-	outfile.write("END WIDTH\n")
+        process_width(outfile, flines, elines)
+        outfile.write("END WIDTH\n")
 
         outfile.close()
-	unidata_file.close()
+        unidata_file.close()
