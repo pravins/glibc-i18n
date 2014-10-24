7beea1ab8866a9afad7259140b5f71c4eda2a9cd
Mike FABIAN <mfabian@redhat.com>
2014-10-24 10:13:55 +0200

    Simplify parsing of the files a bit
    When generating the report with
    
    python3 ctype-compatibility.py ../glibc/localedata/locales/i18n unicode-ctype > Report
    
    this commit does not change the output significantly (i.e. it
    only simplifies the parsing but does not really change anything):
    
    $ diff Report.orig.sorted Report
    21c21
    < combining3: 794 chars in old ctype and 1239 chars in new ctype
    ---
    > combining_level3: 794 chars in old ctype and 1239 chars in new ctype
    41c41
    < printc: 238198 chars in old ctype and 250422 chars in new ctype
    ---
    > print: 238198 chars in old ctype and 250422 chars in new ctype
    55a56,70
    > tolower: 941 chars in old ctype and 1092 chars in new ctype
    > Missing 0 characters of old ctype in new ctype
    >   []
    >
    > ****************************************************
    > totitle: 939 chars in old ctype and 1100 chars in new ctype
    > Missing 0 characters of old ctype in new ctype
    >   []
    >
    > ****************************************************
    > toupper: 949 chars in old ctype and 1100 chars in new ctype
    > Missing 0 characters of old ctype in new ctype
    >   []
    >
    > ****************************************************
    66,71d80
    < Processing for TOUPPER pair group
    < Completed processing for TOUPPER pair group
    < Processing for TOLOWER pair group
    < Completed processing for TOLOWER pair group
    < Processing for TOTITLE pair group
    < Completed processing for TOTITLE pair group
    $

--- ctype-compatibility.py
+++ ctype-compatibility.py
@@ -25,178 +25,135 @@
 
 import os
 import sys
+import re
 
-class ctype:
-    def __init__(self):
-        self.upper, self.lower, self.alpha, self.digit, self.space, self.cntrl, self.punct, \
-            self.graph, self.printc, self.xdigit, self.blank, self.combining, self.combining3  \
-            = [], [], [], [], [], [], [], [], [], [], [], [], []
+def get_lines_from_file(filename):
+    '''Get all non-comment lines from a file
 
-"""Captures Unicode values from character class and add
-to ctype struct
-"""
-def extract_class_and_unichars(filename, struct_ctype):
-    ipfile = open(filename)
-    flines = ipfile.readlines()
-    linecount = len(flines)
-    i = 0
-    for l in flines:
-        w = l.split()
-        if len(w) > 1:
-            if l.split()[0] == "upper":
-#                print("found upper %s" %i)
-                process_chars(i+1, struct_ctype.upper, flines)
-            if l.split()[0] == "lower":
-                process_chars(i+1, struct_ctype.lower, flines)
-            if l.split()[0] == "alpha":
-                process_chars(i+1, struct_ctype.alpha, flines)
-            if l.split()[0] == "digit":
-                process_chars(i+1, struct_ctype.digit, flines)
-            if l.split()[0] == "space":
-                process_chars(i+1, struct_ctype.space, flines)
-            if l.split()[0] == "cntrl":
-                process_chars(i+1, struct_ctype.cntrl, flines)
-            if l.split()[0] == "punct":
-                process_chars(i+1, struct_ctype.punct, flines)
-            if l.split()[0] == "graph":
-                process_chars(i+1, struct_ctype.graph, flines)
-            if l.split()[0] == "print":
-                process_chars(i+1, struct_ctype.printc, flines)
-            if l.split()[0] == "xdigit":
-                process_chars(i+1, struct_ctype.xdigit, flines)
-            if l.split()[0] == "blank":
-                process_chars(i+1, struct_ctype.blank, flines)
-            if l.split()[1] == "\"combining\";":
-#                print("combining")
-                process_chars(i+1, struct_ctype.combining, flines)
-            if l.split()[1] == "\"combining_level3\";":
-#                print("combining_level3")
-                process_chars(i+1, struct_ctype.combining3, flines)
-
-        i = i+1
-#    print("alpha chars group %s" %struct_ctype.alpha)
-
-
-"""Breaks each line of i18n and unicode file into Unicode blocks
-Separated by ";" and extract Unicode values and add into Struct
-"""
-def process_chars(line_no, list_name, flines):
-        for x in range(line_no, len(flines)):
-#               print(x)
-                if len(flines[x].split()) < 1:
-                        break
-                if flines[x].split()[0] == "%":
-                    continue
-
+    Also merge all lines which are continued on the next line because
+    they end in “/” into a single line.
+    '''
+    all_lines = []
+    with open(filename) as file:
+        current_line = ''
+        for line in file:
+            line = line.strip('\n')
+            if '%' in line:
+                if line.endswith('/'):
+                    line = line[0:line.find('%')] + '/'
                 else:
-# Break line into Unicode value range
-                    l = flines[x].strip().split(";")
-                    if l[len(l)-1] != "/":
-                        list_lenght = len(l)+1
-                    else:
-                        list_lenght = len(l)
-                    for i in range (0, list_lenght-1):
-                         individual_word = l[i].split("..")
-#                        print(individual_word)
-# For blocks of characters in  "<UXXXX>..<UXXXX>"
-                         if  len(individual_word ) == 1:
-#                               print(individual_word[0])
-                                uni_char = individual_word[0][2:len(individual_word[0])-1]
-                                hex_uni_char = hex(int(uni_char,16))
-                                list_name.append(hex_uni_char)
-# For blocks of characters in  "<UXXXX>..<UXXXX>"
-                         if  len(individual_word ) == 2:
-#                               print(individual_word)
-                                uni_char1 = individual_word[0][2:len(individual_word[0])-1]
-                                uni_char2 = individual_word[1][2:len(individual_word[0])-1]
-#                               print(int(uni_char1,16), hex(int(uni_char1,16)))
-                                count = 0
-                                for i in range (int(uni_char1,16), int(uni_char2,16)+1):
-                                     list_name.append(hex(int(uni_char1,16)+count))
-                                     count = count + 1
-# For blocks of characters in  "<UXXXX>..(2)..<UXXXX>"
-                         if  len(individual_word ) == 3:
-#                               print(individual_word)
-                                uni_char1 = individual_word[0][2:len(individual_word[0])-1]
-                                uni_char2 = individual_word[2][2:len(individual_word[0])-1]
-#                               print(int(uni_char1,16), hex(int(uni_char1,16)))
-#                               print(int(uni_char2,16), hex(int(uni_char2,16)))
-                                count = 0
-                                for i in range (int(uni_char1,16), int(uni_char2,16)+1,2):
-                                     list_name.append(hex(int(uni_char1,16)+count))
-                                     count = count + 2
-# This condition specifically added for file generated by gen-unicode-ctype.c
-# it does not break char group by line
-                l = flines[x].strip().split(";")
-                if l[len(l)-1] != "/":
-                        break
+                    line = line[0:line.find('%')]
+            line = line.strip()
+            if line.endswith('/'):
+                current_line += line[:-1]
+            else:
+                all_lines.append(current_line + line)
+                current_line = ''
+    return all_lines
 
-# Compared values added in stuct
-def compare_list(old_list, new_list):
-    for property, value in sorted(vars(old_list).items()):
-        prop = eval("new_list.%s" %property)
-        print("%s: %d chars in old ctype and %d chars in new ctype" %(property, len(value), len(prop)))
-        report(value, prop)
+def extract_character_classes_and_code_points(filename):
+    '''Get all Unicode code points for each character class from a file
+
+    Store these code points in a dictionary using the character classes
+    as keys and the list of code points in this character class as values.
+
+    In case  of the character classes “toupper”, “tolower”, and “totitle”,
+    these area actually pairs of code points
+    '''
+    ctype_dict = {}
+    for line in get_lines_from_file(filename):
+        for char_class in [
+                'upper',
+                'lower',
+                'alpha',
+                'digit',
+                'space',
+                'cntrl',
+                'punct',
+                'graph',
+                'print',
+                'xdigit',
+                'blank',
+                'combining',
+                'combining_level3',
+                'toupper',
+                'tolower',
+                'totitle']:
+            match = re.match(r'^(?:(?:class|map)\s+")?'
+                             +char_class+
+                             '(?:";)?\s+', line)
+            if match:
+                if char_class not in ctype_dict:
+                    ctype_dict[char_class] = []
+                process_chars(
+                    ctype_dict[char_class],
+                    line[match.end():])
+    return ctype_dict
+
+def process_chars(char_class_list, code_point_line):
+    '''
+    Extract Unicode values from code_point_line
+    and add to the list of code points in a character class
+    '''
+    for code_points in code_point_line.split(';'):
+        code_points = code_points.strip()
+        match = re.match(r'^<U(?P<codepoint>[0-9A-F]{4,8})>$', code_points)
+        if match: # <UXXXX>
+            char_class_list.append(
+                hex(int(match.group('codepoint'), 16)))
+            continue
+        match = re.match(
+            r'^<U(?P<codepoint1>[0-9A-F]{4,8})>'
+            +'\.\.'+
+            '<U(?P<codepoint2>[0-9A-F]{4,8})>$',
+            code_points)
+        if match: # <UXXXX>..<UXXXX>
+            for codepoint in range(
+                    int(match.group('codepoint1'), 16),
+                    int(match.group('codepoint2'), 16) + 1):
+                char_class_list.append(hex(codepoint))
+            continue
+        match = re.match(
+            r'^<U(?P<codepoint1>[0-9A-F]{4,8})>'
+            +'\.\.\(2\)\.\.'+
+            '<U(?P<codepoint2>[0-9A-F]{4,8})>$',
+            code_points)
+        if match: # <UXXXX>..(2)..<UXXXX>
+            for codepoint in range(
+                    int(match.group('codepoint1'), 16),
+                    int(match.group('codepoint2'), 16) + 1,
+                    2):
+                char_class_list.append(hex(codepoint))
+            continue
+        match = re.match(
+            r'^\('
+            +'<U(?P<codepoint1>[0-9A-F]{4,8})>'
+            +','+
+            '<U(?P<codepoint2>[0-9A-F]{4,8})>'
+            +'\)$',
+            code_points)
+        if match: # (<UXXXX>,<UXXXX>)
+            char_class_list.append((
+                (hex(int(match.group('codepoint1'), 16))),
+                (hex(int(match.group('codepoint2'), 16)))))
+            continue
+
+def compare_lists(old_ctype_dict, new_ctype_dict):
+    for char_class in sorted(old_ctype_dict):
+        print("%s: %d chars in old ctype and %d chars in new ctype" %(
+            char_class,
+            len(old_ctype_dict[char_class]),
+            len(new_ctype_dict[char_class])))
+        report(old_ctype_dict[char_class],
+               new_ctype_dict[char_class])
 
 # Report values to stdout
 def report(old_list, new_list):
-   missing_chars = list(set(old_list)-set(old_list).intersection(set(new_list)))
-   print("Missing %(number)d characters of old ctype in new ctype \n  %(list)s" %{'number': len(missing_chars), 'list': sorted(missing_chars)})
+   missing_chars = list(set(old_list)-set(new_list))
+   print("Missing %(number)d characters of old ctype in new ctype " %{
+       'number': len(missing_chars)})
+   print('  %(list)s' %{'list': sorted(missing_chars)})
    print("\n****************************************************")
-
-
-# This function compares TOLOWER, TOUPPER and TOTITLE pairs of i18n and unicode file
-def check_pairs(file_old, file_new):
-    ipfile = open(file_old)
-    flines = ipfile.readlines()
-    linecount = len(flines)
-    i = 0
-    for l in flines:
-        w = l.split()
-        if len(w) > 1:
-            if l.split()[0] == "toupper":
-                pair_name = "toupper"
-                print("Processing for TOUPPER pair group")
-                process_pairs(i+1, flines, file_new, pair_name)
-                print("Completed processing for TOUPPER pair group")
-            if l.split()[0] == "tolower":
-                pair_name = "tolower"
-                print("Processing for TOLOWER pair group")
-                process_pairs(i+1, flines, file_new, pair_name)
-                print("Completed processing for TOLOWER pair group")
-            if l.split()[1] == "\"totitle\";":
-                pair_name = "totitle"
-                print("Processing for TOTITLE pair group")
-                process_pairs(i+1, flines, file_new, pair_name)
-                print("Completed processing for TOTITLE pair group")
-        i = i + 1
-
-
-""" Split the i18n file line into Unicode pairs and check into
-unicode files
-"""
-def process_pairs(line_no, flines, file_new, pair_name):
-        f = open(file_new).read()
-        for x in range(line_no, len(flines)):
-                if len(flines[x].split()) < 1:
-                        break
-                if flines[x].split()[0] == "%":
-                    continue
-                else:
-# Break line into pairs, separated by ;
-                    l = flines[x].strip().split(";")
-                    if l[len(l)-1] != "/":
-                        list_lenght = len(l)+1
-                    else:
-                        list_lenght = len(l)
-                    for i in range (0, list_lenght-1):
-                        if l[i] not in f:
-                           print("%s not present in %s pair new ctype" %(l[i], pair_name))
-# Groups are not separated by line in unicode file
-                l = flines[x].strip().split(";")
-                if l[len(l)-1] != "/":
-                        break
-
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
@@ -204,9 +161,6 @@ if __name__ == "__main__":
     else:
         file_i18n = sys.argv[1]
         file_unicode = sys.argv[2]
-        ext_ctype = ctype()
-        new_ctype = ctype()
-        extract_class_and_unichars(file_i18n, ext_ctype)
-        extract_class_and_unichars(file_unicode, new_ctype)
-        compare_list(ext_ctype, new_ctype)
-        check_pairs(file_i18n, file_unicode)
+        old_ctype_dict = extract_character_classes_and_code_points(file_i18n)
+        new_ctype_dict = extract_character_classes_and_code_points(file_unicode)
+        compare_lists(old_ctype_dict, new_ctype_dict)
